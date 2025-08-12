5d9508108c1f938c2081f41bfdd269dccb03c716
varun <varunkr9866@gmail.com>
2025-08-12 18:33:11 +0530

    Caught Exception While Establishing Connection In JDBC

--- simplejdbcmysqlexample02/src/com/cluster/Demo.java
+++ simplejdbcmysqlexample02/src/com/cluster/Demo.java
@@ -1,15 +1,17 @@
 package com.cluster;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql","root","Cluster");
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("Caught Exception :" + e);
 		}
 	}
 
