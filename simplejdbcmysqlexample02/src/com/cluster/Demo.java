80d7bee9d50f3d7f2a0fb37afef974e3a9974517
varun <varunkr9866@gmail.com>
2025-08-12 18:38:58 +0530

    Established Connection In JDBC

--- simplejdbcmysqlexample02/src/com/cluster/Demo.java
+++ simplejdbcmysqlexample02/src/com/cluster/Demo.java
@@ -2,7 +2,9 @@ package com.cluster;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Demo {
 
@@ -10,6 +12,10 @@ public class Demo {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql","root","Cluster");
+			Statement st = con.createStatement();
+			ResultSet rs = st.getResultSet();
+			rs.next();
+			
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println("Caught Exception :" + e);
 		}
