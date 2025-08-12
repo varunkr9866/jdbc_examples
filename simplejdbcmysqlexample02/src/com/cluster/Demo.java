fb959a78bedbe32b6a5764172df2256214f84c2e
varun <varunkr9866@gmail.com>
2025-08-12 18:25:56 +0530

    Caught Exception By Using Try Catch

---
+++ simplejdbcmysqlexample02/src/com/cluster/Demo.java
@@ -0,0 +1,16 @@
+package com.cluster;
+
+import java.sql.Connection;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
