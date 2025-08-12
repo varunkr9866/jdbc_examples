bda5511f83044d7493c84c3b8945c73020b2a345
varun <varunkr9866@gmail.com>
2025-08-12 19:11:53 +0530

    Converted Date to String Format By Using DateFormatter

--- simplejdbcmysqlexample02/src/com/cluster/Demo.java
+++ simplejdbcmysqlexample02/src/com/cluster/Demo.java
@@ -1,10 +1,13 @@
 package com.cluster;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
 public class Demo {
 
@@ -13,9 +16,19 @@ public class Demo {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql","root","Cluster");
 			Statement st = con.createStatement();
-			ResultSet rs = st.getResultSet();
-			rs.next();
-			
+			st.execute("INSERT INTO TOPPER_TUTORIALS (STUDENT_ID,STUDENT_NAME ,GENDER ,JOINING_DATE )VALUES('11','RAJ','M','2018-12-21')");
+			st.execute("INSERT INTO TOPPER_TUTORIALS (STUDENT_ID,STUDENT_NAME ,GENDER ,JOINING_DATE )VALUES('12','RANI','F','2018-12-22')");
+			ResultSet rs = st.executeQuery("SELECT * FROM TOPPER_TUTORIALS");
+			while (rs.next()) {
+				String id =rs.getString(1);
+				String name = rs.getString(2);
+				String gender = rs.getString(3);
+				Date date = rs.getDate(4);
+				SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy");
+				 String stringDate = formatter.format(date);
+				System.out.println(id +"/t" + name +"/t" +gender +stringDate);
+				
+			}
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println("Caught Exception :" + e);
 		}
