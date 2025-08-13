671f19caf4dc86579fc14620352c40035d051dea
varun <varunkr9866@gmail.com>
2025-08-13 15:59:31 +0530

    COMPLETED BATCH PROGRAM

--- executemethodsofstatment03/src/com/cluster/ExecuteAddBatch.java
+++ executemethodsofstatment03/src/com/cluster/ExecuteAddBatch.java
@@ -18,9 +18,16 @@ public class ExecuteAddBatch {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql", "root", "Cluster");
 			 st = con.createStatement();
-			 st.addBatch("INSERT INTO TOPPER_TUTORIALS (STUDENT_ID,STUDENT_NAME,GENDER,JOINING_DATE)VALUES('015','TEJA','M','2016-12-31');");
-			 st.addBatch("UPDATE TOPPER_TUTORIALS SET STUDENT_NAME = 'RANA' WHERE STUDENT_ID = '009';");
-			 st.addBatch("DELETE FROM TOPPER_TUTORIALS WHERE STUDENT_ID ='001';");
+			 st.addBatch("INSERT INTO TOPPER_TUTORIALS (STUDENT_ID,STUDENT_NAME,GENDER,JOINING_DATE)VALUES('011','RAMU','M','2016-12-31');");
+			 st.addBatch("UPDATE TOPPER_TUTORIALS SET STUDENT_NAME = 'RAGU' WHERE STUDENT_ID = '009';");
+			 st.addBatch("DELETE FROM TOPPER_TUTORIALS WHERE STUDENT_ID ='015';");
+			 
+			 int a[] = st.executeBatch();
+			 System.out.println("Value of afor executing batch is:"+ a.length);
+			 
+			 for (int i = 0; i < a.length; i++) {
+				System.out.println( a[i]);
+			}
 			 st.executeQuery("SELECT * FROM TOPPER_TUTORIALS;");
 			 rs = st.getResultSet();
 			 while (rs.next()) {
