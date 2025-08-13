bfeab4d74a98d32b873f6f1289594f1e8ad129f0
varun <varunkr9866@gmail.com>
2025-08-13 15:12:33 +0530

    END OF THE PROGRAM

--- executemethodsofstatment03/src/com/cluster/ExecteDemo.java
+++ executemethodsofstatment03/src/com/cluster/ExecteDemo.java
@@ -1,6 +1,7 @@
 package com.cluster;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,12 +18,21 @@ public class ExecteDemo {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql", "root", "Cluster");
 			 st = con.createStatement();
-			 st.execute("INSERT INTO TOPPER_TUTORIALS (STUDENT_ID,STUDENT_NAME,GENDER,JOINING_DATE)VALUES('012','VARUN','M','2016-12-31');");
-			 st.execute("UPDATE TOPPER_TUTORIALS SET STUDENT_NAME = 'AMULYA' WHERE STUDENT_ID = '001';");
+			 st.execute("INSERT INTO TOPPER_TUTORIALS (STUDENT_ID,STUDENT_NAME,GENDER,JOINING_DATE)VALUES('013','PRAHARSHA','F','2016-12-31');");
+			 st.execute("UPDATE TOPPER_TUTORIALS SET STUDENT_NAME = 'KRITIKA' WHERE STUDENT_ID = '002';");
 			 st.execute("DELETE FROM TOPPER_TUTORIALS WHERE STUDENT_ID ='003';");
 			 st.execute("SELECT * FROM TOPPER_TUTORIALS;");
 			 rs = st.getResultSet();
-			 
+			 while (rs.next()) {
+				String id =rs.getString("STUDENT_ID");
+				String name =rs.getString("STUDENT_NAME");
+				String gender =rs.getString("GENDER");
+				Date date =rs.getDate("JOINING_DATE");
+				System.out.println(id);
+				System.out.println(name);
+				System.out.println(gender);
+				System.out.println(date);
+			}
 		} catch (ClassNotFoundException | SQLException e) {
 			
 			e.printStackTrace();
