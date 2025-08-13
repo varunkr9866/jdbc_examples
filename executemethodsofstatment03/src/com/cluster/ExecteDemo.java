cb998696a0d28207309b7e458ac9bed8620fbd2f
varun <varunkr9866@gmail.com>
2025-08-13 15:05:11 +0530

    Completed execute

--- executemethodsofstatment03/src/com/cluster/ExecteDemo.java
+++ executemethodsofstatment03/src/com/cluster/ExecteDemo.java
@@ -3,6 +3,7 @@ package com.cluster;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ExecteDemo {
@@ -16,20 +17,43 @@ public class ExecteDemo {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql", "root", "Cluster");
 			 st = con.createStatement();
-			 st.execute("");
-			 st.execute("");
-			 st.execute("");
-			 st.execute("");
-		} catch (ClassNotFoundException e) {
+			 st.execute("INSERT INTO TOPPER_TUTORIALS (STUDENT_ID,STUDENT_NAME,GENDER,JOINING_DATE)VALUES('012','VARUN','M','2016-12-31');");
+			 st.execute("UPDATE TOPPER_TUTORIALS SET STUDENT_NAME = 'AMULYA' WHERE STUDENT_ID = '001';");
+			 st.execute("DELETE FROM TOPPER_TUTORIALS WHERE STUDENT_ID ='003';");
+			 st.execute("SELECT * FROM TOPPER_TUTORIALS;");
+			 rs = st.getResultSet();
+			 
+		} catch (ClassNotFoundException | SQLException e) {
 			
 			e.printStackTrace();
 		}
-		finally{
-			if (condition) {
-				
+			finally{
+				if (con != null) {
+					try {
+						con.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if (st != null) {
+						try {
+							st.close();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						if (rs != null) {
+							try {
+								rs.close();
+							} catch (SQLException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}
+	
+				}
+	
 			}
-		}
-
-	}
-
+	}			
 }
