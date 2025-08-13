7fb74cd225afbf41b5fb6cb32797e8112837d59e
varun <varunkr9866@gmail.com>
2025-08-13 14:50:44 +0530

    exected method

--- executemethodsofstatment03/src/com/cluster/ExecteDemo.java
+++ executemethodsofstatment03/src/com/cluster/ExecteDemo.java
@@ -1,11 +1,25 @@
 package com.cluster;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ExecteDemo {
 
 	public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
+			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql", "root", "Cluster");
+			 st = con.createStatement();
+			 st.execute("");
+			 st.execute("");
+			 st.execute("");
+			 st.execute("");
 		} catch (ClassNotFoundException e) {
 			
 			e.printStackTrace();
