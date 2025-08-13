01927094684832c9541529c782752bdd3f220452
varun <varunkr9866@gmail.com>
2025-08-13 16:33:31 +0530

    exception caught

--- resultsetexamples04/src/com/cluster/ResultSetDemo.java
+++ resultsetexamples04/src/com/cluster/ResultSetDemo.java
@@ -3,6 +3,12 @@ package com.cluster;
 public class ResultSetDemo {
 
 	public static void main(String[] args) {
+		try {
+			Class .forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 
 	}
