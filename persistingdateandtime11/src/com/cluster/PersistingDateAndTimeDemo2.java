d360a98deaa15a9a8af96f5ae58c4523fce9422a
varun <varunkr9866@gmail.com>
2025-08-17 12:24:53 +0530

    completed the program

--- persistingdateandtime11/src/com/cluster/PersistingDateAndTimeDemo2.java
+++ persistingdateandtime11/src/com/cluster/PersistingDateAndTimeDemo2.java
@@ -1,7 +1,38 @@
 package com.cluster;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.cluster.util.DButil;
+
 public class PersistingDateAndTimeDemo2 {
 		public static void main(String[] args) {
+			Connection con = null;
+			PreparedStatement pst = null;
+			
+			try {
+				con = DButil.getConnection();
+				String date = "31-12-2024";
+				DateFormat dft = new SimpleDateFormat("dd-mm-yyyy");
+				java.util.Date utilDate = dft.parse(date);
+				java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+				
+				String sqlQuery = "INSERT INTO EMP_DOB VALUES(?,?,?)";
+				pst = con.prepareStatement(sqlQuery);
+				pst.setInt(1, 02);
+				pst.setString(2, "NAVVEN");
+				pst.setDate(3, sqlDate);
+				pst.execute();
+				System.out.println("Done !!!!!!!!!!");
+			} catch (ClassNotFoundException | SQLException | ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		}
 }
