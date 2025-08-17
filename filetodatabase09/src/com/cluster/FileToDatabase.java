6d4f75ddd5fda67f05177c7ca1bc87dc9404ef1c
varun <varunkr9866@gmail.com>
2025-08-17 10:13:57 +0530

    Caught Exceptions

--- filetodatabase09/src/com/cluster/FileToDatabase.java
+++ filetodatabase09/src/com/cluster/FileToDatabase.java
@@ -2,14 +2,35 @@ package com.cluster;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
+
+import com.cluster.util.DButil;
 
 public class FileToDatabase {
 
 	public static void main(String[] args) {
+		Scanner sc = null;
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
 		try {
-			Scanner sc = new Scanner(new File("hello.txt"));
-		} catch (FileNotFoundException e) {
+			sc = new Scanner(new File("hello.txt"));
+			con = DButil.getConnection();
+			System.out.println("Got Data Base Connection");
+			st = con.createStatement();
+			
+			while (sc.hasNext()) {
+				int id = sc.nextInt();
+				String name = sc.next();
+				int sal = sc.nextInt();
+				
+			}
+		} catch (FileNotFoundException | ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- filetodatabase09/src/com/cluster/util/DButil.java
+++ filetodatabase09/src/com/cluster/util/DButil.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBuil {
+public class DButil {
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
