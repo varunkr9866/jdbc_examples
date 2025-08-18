a35a629b5c30bcc337c093e6760ebb5d1ba4af9e
varun <varunkr9866@gmail.com>
2025-08-18 12:03:54 +0530

    COMPLETED MINI PROJECT 02

---
+++ JDBCcollections12/src/com/cluster/EmailJDBCCollections.java
@@ -0,0 +1,51 @@
+package com.cluster;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.cluster.util.DButil;
+
+public class EmailJDBCCollections {
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			con = DButil.getConnection();
+			System.out.println("Got The Connection");
+			st = con.createStatement();
+			String mysqlQuery = "SELECT EMAIL FROM EMPLOYEE;";
+			rs = st.executeQuery(mysqlQuery);
+			List<String> listEmail = new ArrayList<>();
+			while (rs.next()) 
+			{
+				listEmail.add(rs.getString("EMAIL"));
+			}
+			Collections.sort(listEmail);
+			for (String strName : listEmail) {
+				System.out.println(strName);
+			}
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("Exception Caught" + e);
+			e.printStackTrace();
+		}
+		finally {
+			if (con != null) {
+				try {
+					con.close();
+					st.close();
+					rs.close();
+				} catch (SQLException e) {
+					System.out.println("Error In Closing The Resources" + e);
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
